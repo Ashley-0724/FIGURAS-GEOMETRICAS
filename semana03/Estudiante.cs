167a245cd3cbabbe8f777e5f375b4e262e6bcdf8
Ashley-0724 <as.alvaradop@uea.edu.ec>
2025-12-14 15:26:31 -0500

    semana3
    semana3

---
+++ semana03/Estudiante.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace semana03
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string[] Telefonos { get; set; }
+
+        public Estudiante(int id, string nombres, string apellidos, string direccion, string[] telefonos)
+        {
+            Id = id;
+            Nombres = nombres;
+            Apellidos = apellidos;
+            Direccion = direccion;
+            Telefonos = telefonos;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("\n--- DATOS DEL ESTUDIANTE ---");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Nombres: {Nombres}");
+            Console.WriteLine($"Apellidos: {Apellidos}");
+            Console.WriteLine($"Dirección: {Direccion}");
+
+            Console.WriteLine("Teléfonos:");
+            for (int i = 0; i < Telefonos.Length; i++)
+            {
+                Console.WriteLine($"Teléfono {i + 1}: {Telefonos[i]}");
+            }
+        }
+    }
+}
--- semana03/Program.cs
+++ semana03/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace semana03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== REGISTRO DE ESTUDIANTE ===");
+
+            Console.Write("Ingrese ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese nombres: ");
+            string nombres = Console.ReadLine();
+
+            Console.Write("Ingrese apellidos: ");
+            string apellidos = Console.ReadLine();
+
+            Console.Write("Ingrese dirección: ");
+            string direccion = Console.ReadLine();
+
+            string[] telefonos = new string[3];
+
+            for (int i = 0; i < telefonos.Length; i++)
+            {
+                Console.Write($"Ingrese teléfono {i + 1}: ");
+                telefonos[i] = Console.ReadLine();
+            }
+
+            Estudiante estudiante = new Estudiante(id, nombres, apellidos, direccion, telefonos);
+            estudiante.MostrarDatos();
+
+            Console.ReadKey();
+        }
+    }
+}
